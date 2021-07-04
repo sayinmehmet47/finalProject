2675e67f90839bdf6c12d4fc8b59496595562f29
sayin47 <sayinmehmet47@gmail.com>
2021-07-04 17:53:12 +0300

    added cards

---
+++ script.js
@@ -0,0 +1,18 @@
+var open = 0;
+var $;
+
+var mySidebar = document.getElementById('mySidebar');
+function myFunction() {
+  if (open === 0) {
+    mySidebar.style.width = '250px';
+    return (open = 1);
+  }
+  if (open === 1) {
+    mySidebar.style.width = '0px';
+    return (open = 0);
+  }
+}
+$ = require('jquery');
+$('.tag').click(function () {
+  return console.log('clicked');
+});
